d3d63d46b128c3cd0bf6dae2663c63ea5c214f24
Derek Emmanuel <derekemmanuel99@gmail.com>
2023-09-16 09:56:20 +0100

    new commit

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-<<<<<<< HEAD
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendar, faHome, faTv, faVideo } from '@fortawesome/free-solid-svg-icons';
 
@@ -40,64 +39,10 @@ function MovieDetails({ match }) {
           TV Series
         </p>
         <p className='p-2 bg-orange-400 rounded-md'>
-=======
-import AddFavourites from "./AddFavourites";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCalendar,
-  faHome,
-  faTv,
-  faVideo,
-} from "@fortawesome/free-solid-svg-icons";
-import { Link, useParams } from "react-router-dom";
-import axios from "axios";
-
-const MovieDetails = () => {
-  const [details, setDetails] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const { id } = useParams();
-  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=767a7cc8de7d102579d3d1cf9acd65e8`;
-
-  useEffect(() => {
-    setLoading(true);
-    axios
-      .get(url)
-      .then((res) => {
-        setLoading(false);
-        setDetails(res?.data);
-        setError(null);
-      })
-      .catch((err) => {
-        setLoading(false);
-        setError(err.message);
-      });
-  }, [url]);
-
-  return (
-    <div>
-      <div>
-        <Link to="/">
-          <p className="p-2 bg-orange-400 rounded-md">
-            <FontAwesomeIcon icon={faHome} />
-            Home
-          </p>
-        </Link>
-        <p className="p-2 bg-orange-400 rounded-md">
-          <FontAwesomeIcon icon={faVideo} />
-          Movie
-        </p>
-        <p className="p-2 bg-orange-400 rounded-md">
-          <FontAwesomeIcon icon={faTv} />
-          TV Series
-        </p>
-        <p className="p-2 bg-orange-400 rounded-md">
->>>>>>> edcf314e4049c1fe5d97faf496682a47d3cc4e88
           <FontAwesomeIcon icon={faCalendar} />
           Upcoming
         </p>
       </div>
-<<<<<<< HEAD
       <div>
         <h1>{movieDetails.title}</h1>
         <p>{movieDetails.overview}</p>
@@ -106,13 +51,5 @@ const MovieDetails = () => {
     </div>
   );
 }
-=======
-      {loading && <p>Loading......</p>}
-      {error && <p>{error}</p>}
-      {details && <h1>{details.original_title}</h1>}
-    </div>
-  );
-};
->>>>>>> edcf314e4049c1fe5d97faf496682a47d3cc4e88
 
 export default MovieDetails;
